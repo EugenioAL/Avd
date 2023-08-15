1b05d1c63eeeccb0b81be5b4f3fce498c5811247
eugenio <eaof@icomp.ufam.edu.br>
2023-08-15 11:42:59 -0400

    repush

--- avdLib.py
+++ avdLib.py
@@ -25,5 +25,37 @@ def mediaPond(matriz):
     for i in range(len(matriz[1])):
         valorTotal += matriz[0][i]*matriz[1][i]
     return valorTotal/pesoTotal
+#----------------------To do---------------------------------
+#def mediaGeometrica
+#def mediaHarmonica
+#def mediaTaxas
+#--exercicio slide 18Exercício
+#def mediana
+#def moda
+#def amplitude
+#--exercicio slide 29
+#--exercicio slide 30
+#--exercicio slide 31
+#def desvioPadrao
+#--exercicio slide 33
+#def coefVariacao
+##--exercicio slide 35
+#def quartis
+##--exercicio slide 42
+#def intevaloConfiancaMedia
+#--exercicio slide 51
+#--exercicio slide 54
+#def testeMediazero
+#--exercicio slide 58
+#--exercicio slide 59
+#--exercicio slide 62
+#def tamanhoAmostra
+#--exercicio slide 67
+#--exercicio slide 69
+#def testeHipotese
+#def testeShapWilk
+#--exercicio slide 86
+#
+#
+#
 
-def mediaHarmo
