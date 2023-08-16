d1e6fc92129ef2dae5e357cd1220ea146fb3c153
eugenioal <eaof@icomp.ufam.edu.br>
2023-08-16 09:54:44 -0400

    v2

--- avdLib.py
+++ avdLib.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import functools as ft
+from math import *
 
 
 def arrayFfile(nome):
@@ -25,14 +26,39 @@ def mediaPond(matriz):
     for i in range(len(matriz[1])):
         valorTotal += matriz[0][i]*matriz[1][i]
     return valorTotal/pesoTotal
+
+def mediaGeometrica(vetor):
+    acumulador = 1
+    for i in range(len(vetor)):
+        acumulador*=vetor[i]
+    return acumulador**(1/len(vetor))
+    
+def amplitude(vetor):
+    min = min(vetor)
+    max = max(vetor)
+    return max-min
+
+def mediaTaxas(vetorA, vetorB):
+    media(vetorA/vetorB)
+
+def mediana(vetor):
+    vetor_ordenado = sorted(vetor)
+    tamanho = len(vetor_ordenado)
+
+    if tamanho % 2 == 0:
+        mediana = (vetor_ordenado[tamanho // 2-1] + vetor_ordenado[tamanho //2])/2
+    else:
+        mediana = vetor_ordenado[tamanho // 2]
+    return mediana
+    
 #----------------------To do---------------------------------
-#def mediaGeometrica
+
+
 #def mediaHarmonica
 #def mediaTaxas
 #--exercicio slide 18Exercício
 #def mediana
 #def moda
-#def amplitude
 #--exercicio slide 29
 #--exercicio slide 30
 #--exercicio slide 31
